4afdff85cb75308bbe40f9ce57f92a89e6dbed39
kallejre <kallejohannes.reinmets@real.edu.ee>
2017-08-16 00:59:21 +0300

    Neljadimensioonilise värviruumi tugi
    Teisisõnu värvib programm nüüd RGBA ja HSVA funktsioonide süntaksit, kui nende sisu sõltub muutuja t (aeg) väärtusest.
    -Kalle

--- editor/gui.py
+++ editor/gui.py
@@ -1,8 +1,11 @@
-
 import gi #type: ignore
 from gi.repository import GLib,Gtk,Gdk, GObject, GtkSource #type: ignore
 import string
 import colorsys
+import math
+from math import *
+import os,sys
+MULTICOLOR_KILL_SWITCH=True
 
 builder = Gtk.Builder().new_from_file('gui.glade')
 
@@ -61,27 +64,48 @@ def try_parse_color(text):
     # example 'RGBA( 1.0 , 0.0 , 0.1 )'
     text = text.replace(' ', '')
     try:
-        numbers = list(map(float, text[5:-1].split(',')))
-        assert len(numbers) < 5
-        if text[ :4] == 'RGBA':
-            numbers = map(lambda x: 0 if x<0 else (x if x<1 else 1), numbers)
-            r, g, b, *_ = numbers
-        elif text[ :4] == 'HSVA':
-            s, v = 1, 1
-            h = numbers[0]
-            try:
-                s = numbers[1]
-                v = numbers[2]
-                s, v = map(lambda x: 0 if x<0 else (x if x<1 else 1), [s,v])
-            except Exception: pass
-            r, g, b = colorsys.hsv_to_rgb(h, s, v)
-        else: assert False
-    except Exception:
+        l=len(text)
+        step=round(10/l,2)
+        #print(text,l,step)
+        rgb=[]
+        args=text[5:-1] #funktsiooni argumendid
+        ttt=0.0
+        for i in range(l):
+            if MULTICOLOR_KILL_SWITCH:
+                global t # Lüliti tööpõhimõte: kui programm ei leia muutujat t, väljastab vea
+                t=ttt # Kui funk. ei kasuta t-d, pole probleemi.
+            numbers = list(map(lambda x:float(eval(x)), args.split(',')))
+            ttt+=step
+            assert len(numbers) < 5
+            if text[ :4] == 'RGBA':
+                numbers = map(lambda x: 0 if x<0 else (x if x<1 else 1), numbers)
+                r, g, b, *_ = numbers
+                rgb.append((r,g,b))
+            elif text[ :4] == 'HSVA':
+                s, v = 1, 1
+                h = numbers[0]
+                try:
+                    s = numbers[1]
+                    v = numbers[2]
+                    s, v = map(lambda x: 0 if x<0 else (x if x<1 else 1), [s,v])
+                except Exception: pass
+                r, g, b = colorsys.hsv_to_rgb(h, s, v)
+                #rgb.append((r**0.3,g**0.3,b**0.3))
+                rgb.append((r,g,b))
+            else: assert False
+    except Exception as e:
+        
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
+        print(e)
         return None
-    return r, g, b
+    return rgb
+
 
 def get_fg_and_bg(r, g, b):
-    if r+g+b > 1.5:
+    l=0.299*r+0.587*g+0.114*b
+    if l > 0.5:
         fg = Gdk.RGBA(0,0,0,1)
     else:
         fg = Gdk.RGBA(1,1,1,1)
@@ -109,14 +133,28 @@ def on_highlight_updated(buffer, start, end):
         if not found:
             break
         current_iter = end
-        rgb = try_parse_color(buffer.get_text(start, end, True))
+        while True:
+            tex=buffer.get_text(start, end, True)
+            if tex.count('(')!=tex.count(')'):
+                end.forward_char()
+            else:
+                break
+            if end.ends_line():
+                break
+        tex=buffer.get_text(start, end, True)
+        rgb = try_parse_color(tex)
         if rgb != None:
-            fg, bg = get_fg_and_bg(*rgb)
-            new_tag = buffer.create_tag(foreground_rgba = fg,
-                                        background_rgba = bg,
-                                        weight = 700)
-            buffer.apply_tag(new_tag, start, end)
-            color_tags.append(new_tag)
+            start2=start.copy()
+            start2.forward_char()
+            for i in rgb:
+                fg, bg = get_fg_and_bg(*i)
+                new_tag = buffer.create_tag(foreground_rgba = fg,
+                                            background_rgba = bg,
+                                            weight = 700)
+                buffer.apply_tag(new_tag, start, start2)
+                start.forward_char()
+                start2.forward_char()
+                color_tags.append(new_tag)
     print('HIGHLIGHT UPDATED')
 
 input_buffer.connect('highlight-updated', on_highlight_updated)
